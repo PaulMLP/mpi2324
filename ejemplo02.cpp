b9c94f37de5de87f8aa254a6fb0a68562ac6fc0f
Paul Merizalde <merizaldepaul1999@gmail.com>
2024-01-16 13:41:58 +0000

    coomit

--- ejemplo02.cpp
+++ ejemplo02.cpp
@@ -13,21 +13,24 @@ int main(int argc, char** argv){
     int data[100]; //vector de tamaño 100 sin inicializar
 
     if(rank == 0){
+        std::printf("Total ranks: %d\n", nprocs);
         //inicializamos vector
         for (int i=0; i<100;i++) {
             data[i]= i;
         }
 
-        std::printf("Rank %d enviando datos\n", rank);
-        MPI_Send(data,      //datos
-                 100,       //count
-                 MPI_INT,   //tipo de dato
-                 1,         //rank destino
-                 0,         //tag
-                 MPI_COMM_WORLD //grupo
-                 );
-    } else if (rank == 1){
-        std::printf("Rank %d recibiendo datos\n", rank);
+        for (int i = 1; i < nprocs; ++i) {
+            std::printf("Rank_%d enviando datos a Rank_%d\n", rank, i);
+            MPI_Send(data,      //datos
+                     100,       //count
+                     MPI_INT,   //tipo de dato
+                     i,         //rank destino
+                     0,         //tag
+                     MPI_COMM_WORLD //grupo
+            );
+        }
+    } else {
+
         MPI_Recv(data,
                  100,       //count
                  MPI_INT,   //tipo de dato
@@ -37,12 +40,14 @@ int main(int argc, char** argv){
                  MPI_STATUS_IGNORE //status
                  );
 
-        for (int i = 0; i < 100; ++i) {
-            std::printf("%d, ", data[i]);
+        std::string str = "";
+        for (int i = 0; i < 10; ++i) {
+            //std::printf("%d, ", data[i]);
+            str = str + std::to_string(data[i])+",";
         }
-        std::printf("\n");
+        std::printf("Rank_%d recibiendo datos ===> %s\n",rank, str.c_str());
+        //std::printf("\n");
     }
-    //std::printf("Rank %d of %d process \n", rank, nprocs);
 
     MPI_Finalize();
     return  0;
